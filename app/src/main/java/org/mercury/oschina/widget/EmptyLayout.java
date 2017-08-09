e6f259326ffa242d8be42a0df17be670e66c7d89
mercuryglory <wzhseu@163.com>
2017-08-09 20:14:07 +0800

    use TabLayout to replace PagerSlidingTabStrip

--- app/src/main/java/org/mercury/oschina/widget/EmptyLayout.java
+++ app/src/main/java/org/mercury/oschina/widget/EmptyLayout.java
@@ -2,77 +2,56 @@ package org.mercury.oschina.widget;
 
 import android.content.Context;
 import android.util.AttributeSet;
+import android.view.LayoutInflater;
 import android.view.View;
+import android.widget.FrameLayout;
 import android.widget.ImageView;
-import android.widget.LinearLayout;
-import android.widget.RelativeLayout;
+import android.widget.ProgressBar;
 import android.widget.TextView;
-
-import net.oschina.common.widget.Loading;
 
 import org.mercury.oschina.R;
 import org.mercury.oschina.utils.TDevice;
 
+public class EmptyLayout extends FrameLayout implements
+        android.view.View.OnClickListener {
 
-public class EmptyLayout extends LinearLayout implements
-        View.OnClickListener {// , ISkinUIObserver {
-
-    public static final int HIDE_LAYOUT = 4;
-    public static final int NETWORK_ERROR = 1;
-    public static final int NETWORK_LOADING = 2;
-    public static final int NODATA = 3;
+    public static final int NETWORK_ERROR       = 1;
+    public static final int NETWORK_LOADING     = 2;
+    public static final int NODATA              = 3;
+    public static final int HIDE_LAYOUT         = 4;
     public static final int NODATA_ENABLE_CLICK = 5;
-    public static final int NO_LOGIN = 6;
+    public static final int NO_LOGIN            = 6;
 
-    private Loading mLoading;
+    private ProgressBar mLoading;
     private boolean clickEnable = true;
-    private final Context context;
-    public ImageView img;
-    private OnClickListener listener;
-    private int mErrorState;
+    public        ImageView                         img;
+    private       android.view.View.OnClickListener listener;
+    private       int                               mErrorState;
     private String strNoDataContent = "";
     private TextView tv;
 
     public EmptyLayout(Context context) {
         super(context);
-        this.context = context;
         init();
     }
 
     public EmptyLayout(Context context, AttributeSet attrs) {
         super(context, attrs);
-        this.context = context;
         init();
     }
 
     private void init() {
-        View view = View.inflate(context, R.layout.view_error_layout, null);
+        View view = LayoutInflater.from(getContext()).inflate(R.layout.view_error_layout, this,
+                false);
         img = (ImageView) view.findViewById(R.id.img_error_layout);
         tv = (TextView) view.findViewById(R.id.tv_error_layout);
-        RelativeLayout mLayout = (RelativeLayout) view.findViewById(R.id.pageerrLayout);
-        mLoading = (Loading) view.findViewById(R.id.animProgress);
+        mLoading = (ProgressBar) view.findViewById(R.id.animProgress);
         setBackgroundColor(-1);
         setOnClickListener(this);
-        img.setOnClickListener(new OnClickListener() {
 
-            @Override
-            public void onClick(View v) {
-                if (clickEnable) {
-                    // setErrorType(NETWORK_LOADING);
-                    if (listener != null)
-                        listener.onClick(v);
-                }
-            }
-        });
         addView(view);
-        changeErrorLayoutBgMode(context);
     }
 
-    public void changeErrorLayoutBgMode(Context context1) {
-        // mLayout.setBackgroundColor(SkinsUtil.getColor(context1,
-        // "bgcolor01"));
-        // tv.setTextColor(SkinsUtil.getColor(context1, "textcolor05"));
-    }
 
     public void dismiss() {
         mErrorState = HIDE_LAYOUT;
@@ -100,25 +79,6 @@ public class EmptyLayout extends LinearLayout implements
         }
     }
 
-    @Override
-    protected void onAttachedToWindow() {
-        super.onAttachedToWindow();
-        // AppContext.getInstance().getAtSkinObserable().registered(this);
-        onSkinChanged();
-    }
-
-    @Override
-    protected void onDetachedFromWindow() {
-        super.onDetachedFromWindow();
-        // AppContext.getInstance().getAtSkinObserable().unregistered(this);
-    }
-
-    public void onSkinChanged() {
-        // mLayout.setBackgroundColor(SkinsUtil
-        // .getColor(getContext(), "bgcolor01"));
-        // tv.setTextColor(SkinsUtil.getColor(getContext(), "textcolor05"));
-    }
-
     public void setErrorMessage(String msg) {
         tv.setText(msg);
     }
@@ -140,48 +100,39 @@ public class EmptyLayout extends LinearLayout implements
         switch (i) {
             case NETWORK_ERROR:
                 mErrorState = NETWORK_ERROR;
-                // img.setBackgroundDrawable(SkinsUtil.getDrawable(context,"pagefailed_bg"));
                 if (TDevice.hasInternet()) {
                     tv.setText(R.string.error_view_load_error_click_to_refresh);
-                    img.setBackgroundResource(R.mipmap.pagefailed_bg);
+                    img.setBackgroundResource(R.drawable.ic_tip_fail);
                 } else {
                     tv.setText(R.string.error_view_network_error_click_to_refresh);
                     img.setBackgroundResource(R.mipmap.page_icon_network);
                 }
                 img.setVisibility(View.VISIBLE);
-                mLoading.stop();
                 mLoading.setVisibility(View.GONE);
                 clickEnable = true;
                 break;
             case NETWORK_LOADING:
                 mErrorState = NETWORK_LOADING;
-                // mLoading.setBackgroundDrawable(SkinsUtil.getDrawable(context,"loadingpage_bg"));
                 mLoading.setVisibility(View.VISIBLE);
-                mLoading.start();
                 img.setVisibility(View.GONE);
                 tv.setText(R.string.error_view_loading);
                 clickEnable = false;
                 break;
             case NODATA:
                 mErrorState = NODATA;
-                // img.setBackgroundDrawable(SkinsUtil.getDrawable(context,"page_icon_empty"));
-                img.setBackgroundResource(R.mipmap.page_icon_empty);
+                img.setBackgroundResource(R.drawable.ic_tip_fail);
                 img.setVisibility(View.VISIBLE);
-                mLoading.stop();
                 mLoading.setVisibility(View.GONE);
                 setTvNoDataContent();
                 clickEnable = true;
                 break;
             case HIDE_LAYOUT:
-                mLoading.stop();
                 setVisibility(View.GONE);
                 break;
             case NODATA_ENABLE_CLICK:
                 mErrorState = NODATA_ENABLE_CLICK;
                 img.setBackgroundResource(R.mipmap.page_icon_empty);
-                // img.setBackgroundDrawable(SkinsUtil.getDrawable(context,"page_icon_empty"));
                 img.setVisibility(View.VISIBLE);
-                mLoading.stop();
                 mLoading.setVisibility(View.GONE);
                 setTvNoDataContent();
                 clickEnable = true;
@@ -195,7 +146,7 @@ public class EmptyLayout extends LinearLayout implements
         strNoDataContent = noDataContent;
     }
 
-    public void setOnLayoutClickListener(OnClickListener listener) {
+    public void setOnLayoutClickListener(View.OnClickListener listener) {
         this.listener = listener;
     }
 
